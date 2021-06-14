8a5e16e50779c479ee16948f68aedb69a259bc97
kavyagoel26 <75258398+kavyagoel26@users.noreply.github.com>
2021-06-14 13:50:56 +0530

    Add files via upload

---
+++ quick sort.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+void swap(int arr[], int i, int j)
+{
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+int partition(int arr[], int l, int r)
+{
+    int pivot = arr[r];
+    int i = i - 1;
+    for (int j = i; j < r; j++)
+    {
+        if (arr[j] < arr[r])
+        {
+            i++;
+            swap(arr, i, j);
+        }
+    }
+    swap(arr, i + 1, r);
+    return i + 1;
+}
+void quicksort(int arr[], int l, int r)
+{
+    if (l < r)
+    {
+        int pi = partition(arr, l, r);
+        quicksort(arr, l, pi - 1);
+        quicksort(arr, pi + 1, r);
+    }
+}
+int main()
+{
+    int arr[] = {5, 4, 3, 2, 1};
+    quicksort(arr, 0, 4);
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
